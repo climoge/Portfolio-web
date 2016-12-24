0629e00dd71b5862c94a42df704912aec6209cb8
climoge <corentin.limoge@gmail.com>
2016-12-24 13:26:21 +0100

    Change favicon and title style

--- phpBB3/cache/sql_f2afcd9dca7b065185632af0109d94ee.php
+++
@@ -1,5 +0,0 @@
-<?php exit; ?>
-1482420609
-SELECT * FROM phpbb_styles s WHERE s.style_id = 2
-292
-a:1:{i:0;a:8:{s:8:"style_id";s:1:"2";s:10:"style_name";s:12:"ComBoot Free";s:15:"style_copyright";s:22:"© Florian Gareis 2016";s:12:"style_active";s:1:"1";s:10:"style_path";s:11:"ComBoot-3.1";s:15:"bbcode_bitfield";s:4:"+Ng=";s:15:"style_parent_id";s:1:"0";s:17:"style_parent_tree";s:0:"";}}
